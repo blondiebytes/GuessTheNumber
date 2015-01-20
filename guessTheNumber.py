b796546e73c0196cd394ee58d3730d130868801c
Kathryn Hodge <kahodge@vassar.edu>
2015-01-19 21:07:19 -0600

    Fixed dialogue for the sassy number ninja

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -7,41 +7,42 @@ import sys
 
 
 #TO DO - Figure out a way to get rid of casting; instead check if number, then cast?
-#      - Put the text in functions that return strings
 
 
 #FUNCTIONS
 
 def welcomeText() :
-    return "My name is The Number NINJA and I doubt you can guess what\n"
-    "number I\'m thinking of. It\'s between 0 and 99, but what it is... \n"
-    "you\'ll never know. So go ahead, take a guess... and fail."
+    return "My name is The Number NINJA and I doubt you can guess " \
+           "what\nnumber I\'m thinking of. It\'s between 0 and 99, " \
+           "but what it is... \nyou\'ll never know. So go ahead, " \
+           "take a guess... and fail."
 
 
 def userTooBig() :
     randomNum = random.randrange(0,1000)
     if randomNum % 2 == 0 :
-        "Ha, ha. Think smaller! Or you will have no chance in figuring it out."
+        return "Ha, ha. Think smaller! Or you will have no chance in figuring it out."
     else:
-        "Wrong wrong wrong! More isn't always better"
+        return "Wrong wrong wrong! More isn't always better"
 
 
 def userTooSmall() :
     randomNum = random.randrange(0,1000)
     if randomNum % 2 == 0:
-        "Ha, ha you are wrong! My number is way bigger than you think!"
+       return "Ha, ha you are wrong! My number is way bigger than you think!"
     else:
-        "Nope!! You underestimate the power of my NUMBER"
+        return "Nope!! You underestimate the power of my NUMBER!"
 
 
 def goodByeText(my_num) :
-    return "No, no, no! You figured out my secret number, %d! You must be some kind of mastermind \n"
-    "in the works. Grrr....Go ahead. Tell all your friends you have beaten me, the Master of \n" \
+    return "No, no, no! You figured out my secret number, %d! You must be some kind of mastermind \n" \
+    "in the works. Grrr.... Go ahead. Tell all your friends you have beaten me, the Master of \n" \
     "All Numbers. You are now the new Number Ninja!"%(my_num)
 
 
 my_number = random.randrange(0,100)
 
+# What we actually run....
 print(welcomeText())
 userInput = int(sys.stdin.readline())
 while userInput != my_number:
