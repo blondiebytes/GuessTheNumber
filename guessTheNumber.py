ac824f0b3aeb9686a6e9b4052d971c4c24e6fc61
Kathryn Hodge <kahodge@vassar.edu>
2015-01-20 12:00:44 -0600

    Re-organizing exceptions and type checking

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -11,6 +11,7 @@ import sys
 
 #FUNCTIONS
 
+# Message: Welcoming Text
 def welcomeText() :
     return "My name is The Number NINJA and I doubt you can guess " \
            "what\nnumber I\'m thinking of. It\'s between 0 and 99, " \
@@ -18,6 +19,7 @@ def welcomeText() :
            "take a guess... and fail."
 
 
+# Message: if the user's guess is too big
 def userTooBig() :
     randomNum = random.randrange(0,1000)
     if randomNum % 2 == 0 :
@@ -25,7 +27,7 @@ def userTooBig() :
     else:
         return "Wrong wrong wrong! More isn't always better"
 
-
+# Message: if the user's guess is too small
 def userTooSmall() :
     randomNum = random.randrange(0,1000)
     if randomNum % 2 == 0:
@@ -33,40 +35,47 @@ def userTooSmall() :
     else:
         return "Nope!! You underestimate the power of my NUMBER!"
 
+# Customized Type Checking
 def isInputNum(user_input):
     return user_input.isdigit
 
 def inputChecker(user_input):
     if not isInputNum(user_input):
-        raise Exception("Ha, ha, you don't even know what a number is!")
+        raise Exception("Invalid Input")
 
+# Type Checker / Throw Exception(er)
+def inputCheckerV2(userInput):
+    try:
+        userInput2 = int(userInput)
+    except ValueError:
+        print("Ha, ha, you don't even know what a number is! YOU LOSE and I shall be the Number Ninja"
+              " forever! MUAHAHAHAHAHA! ")
+        raise
+
+# Message: Defeat Text
 def goodByeText(my_num) :
     return "No, no, no! You figured out my secret number, %d! You must be some kind of mastermind \n" \
     "in the works. Grrr.... Go ahead. Tell all your friends you have beaten me, the Master of \n" \
     "All Numbers. You are now the new Number Ninja!"%(my_num)
 
 
-my_number = random.randrange(0,100)
+ninja_number = random.randrange(0, 100)
 
 # What we actually run....
 print(welcomeText())
 userInput = sys.stdin.readline()
-try:
-    checkInput = inputChecker(userInput)
-except Exception:
-    print("Ha, ha, you don't even know what a number is!")
-
+inputCheckerV2(userInput)
 userInput = int(userInput)
-while userInput != my_number:
-    if userInput < my_number:
+while userInput != ninja_number:
+    if userInput < ninja_number:
         print(userTooSmall())
         userInput = sys.stdin.readline()
-        checkInput = inputChecker(userInput)
+        inputCheckerV2(userInput)
         userInput = int(userInput)
     else :
         print(userTooBig())
         userInput = sys.stdin.readline()
-        checkInput = inputChecker(userInput)
+        inputCheckerV2(userInput)
         userInput = int(userInput)
 
-print(goodByeText(my_number))
+print(goodByeText(ninja_number))
